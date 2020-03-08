c8bb32bc0aad6c55231cb2810665128ed33d8d4a
Recessive <aa.mindustry@gmail.com>
2020-03-09 01:06:59 +1100

    Updated banned names to include "server"

--- src/main/java/hexed/BannedNames.java
+++ src/main/java/hexed/BannedNames.java
@@ -3,5 +3,5 @@ package hexed;
 //TODO put in other plugin
 public class BannedNames{
     //this is probably the most curses I've ever typed
-    public static final String[] badNames = {"shit", "fuck", "asshole", "cunt", "nigger", "nigga", "niga", "faggot", "dick", "bitch", "admin", "mod", "mindustry", "owner", "<", ">"};
+    public static final String[] badNames = {"shit", "fuck", "asshole", "cunt", "nigger", "nigga", "niga", "faggot", "dick", "bitch", "admin", "mod", "mindustry", "server", "owner", "<", ">"};
 }
